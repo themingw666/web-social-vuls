5d425aaf03d1a9b7a2a32b29384eeade14f73256
Ming <themingw666@gmail.com>
2024-07-22 09:06:55 +0700

    forgot-password function part 2 - check and send password

--- src/controller/forgotpasswordControllers.js
+++ src/controller/forgotpasswordControllers.js
@@ -9,13 +9,28 @@ const handle = async (req,res) => {
     return res.redirect('/forgot-password/vertify')
 }
 
-const vertify = async (req,res) => {
-    const email = req.session.email;
-    console.log(email);
+const index_vertify = async (req,res) => {
     return res.render('forgot-password-vertify', { layout: false })
 }
 
-const reset = async (req,res) => {
+const handle_vertify = async (req,res) => {
+    //check password
+    const password = req.body.password;
+    if (password !== null || password !== "") {
+        return res.redirect('/forgot-password/reset')
+    }
+    else {
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+const index_reset = async (req,res) => {
+    return res.render('forgot-password-reset', { layout: false })
+}
+
+const handle_reset = async (req,res) => {
+    const email = req.session.email;
+    console.log(email);
     return res.render('forgot-password-reset', { layout: false })
 }
 
@@ -37,4 +52,4 @@ const check = async (req,res) => {
     }
 }
 
-export default {index, handle, vertify, reset, check}
+export default {index, handle, index_vertify, handle_vertify, index_reset, handle_reset, check}
--- src/routes/forgotpasswordRoute.js
+++ src/routes/forgotpasswordRoute.js
@@ -2,23 +2,26 @@ import express from "express";
 const Route = express.Router()
 import forgotpasswordController from "../controller/forgotpasswordControllers.js"
 import session from 'express-session'
+
 Route.use(session({
     secret: '666666',
     resave: false,
     saveUninitialized: true
 }));
 
-const emailMiddleware = (req, res, next) => {
+const dataMiddleware = (req, res, next) => {
     if (req.body.email) {
         req.session.email = req.body.email
     }
     next();
 };
 
-Route.get('/vertify',emailMiddleware, forgotpasswordController.vertify)
-Route.get('/reset',forgotpasswordController.reset)
+Route.get('/vertify', dataMiddleware, forgotpasswordController.index_vertify)
+Route.post('/vertify', dataMiddleware, forgotpasswordController.handle_vertify)
+Route.get('/reset', dataMiddleware, forgotpasswordController.index_reset)
+Route.post('/reset', dataMiddleware, forgotpasswordController.handle_reset)
 Route.post('/check', forgotpasswordController.check)
 Route.get('/',forgotpasswordController.index)
-Route.post('/', emailMiddleware, forgotpasswordController.handle)
+Route.post('/', dataMiddleware, forgotpasswordController.handle)
 
 export default Route
