b83245efc80c578389fc49f395d1fdaeba399d23
Ming <themingw666@gmail.com>
2024-07-22 07:29:26 +0700

    forgot-password function part 1 - check and send email

--- src/controller/forgotpasswordControllers.js
+++ src/controller/forgotpasswordControllers.js
@@ -1,8 +1,17 @@
+import {PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 const index = async (req,res) => {
     return res.render('forgot-password', { layout: false })
 }
 
+const handle = async (req,res) => {
+    return res.redirect('/forgot-password/vertify')
+}
+
 const vertify = async (req,res) => {
+    const email = req.session.email;
+    console.log(email);
     return res.render('forgot-password-vertify', { layout: false })
 }
 
@@ -10,4 +19,22 @@ const reset = async (req,res) => {
     return res.render('forgot-password-reset', { layout: false })
 }
 
-export default {index, vertify, reset}
+const check = async (req,res) => {
+    try {
+        const { email } = req.body;
+        const data = await prisma.user.findUnique({
+            where: {
+            email: email,
+            },
+        })
+        
+        const emailExists = (data !== null)
+        return res.json({ emailExists: emailExists });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+export default {index, handle, vertify, reset, check}
--- src/routes/forgotpasswordRoute.js
+++ src/routes/forgotpasswordRoute.js
@@ -1,9 +1,24 @@
 import express from "express";
 const Route = express.Router()
 import forgotpasswordController from "../controller/forgotpasswordControllers.js"
+import session from 'express-session'
+Route.use(session({
+    secret: '666666',
+    resave: false,
+    saveUninitialized: true
+}));
 
-Route.get('/vertify',forgotpasswordController.vertify)
+const emailMiddleware = (req, res, next) => {
+    if (req.body.email) {
+        req.session.email = req.body.email
+    }
+    next();
+};
+
+Route.get('/vertify',emailMiddleware, forgotpasswordController.vertify)
 Route.get('/reset',forgotpasswordController.reset)
+Route.post('/check', forgotpasswordController.check)
 Route.get('/',forgotpasswordController.index)
+Route.post('/', emailMiddleware, forgotpasswordController.handle)
 
 export default Route
